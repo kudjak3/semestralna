eb04d01ec70556bd997bbafabdd6eb599e675e34
kudjak3 <kudjak3@stud.uniza.sk>
2021-02-11 07:24:57 +0100

    commenty migracie

--- database/migrations/2021_01_20_074407_create_blogs_table.php
+++ database/migrations/2021_01_20_074407_create_blogs_table.php
@@ -14,7 +14,7 @@ class CreateBlogsTable extends Migration
     public function up()
     {
         Schema::create('blogs', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->text('title');
             $table->text('text');
             $table->unsignedInteger('user_id');
